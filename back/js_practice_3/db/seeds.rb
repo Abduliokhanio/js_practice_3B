7cef93b190d33567754ee2b38cd685618aa11bd0
Abduliokhanio <54249636+Abduliokhanio@users.noreply.github.com>
2020-10-11 22:45:37 -0500

    Add files via upload

---
+++ back/js_practice_3/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+Gif.create(title: 'wild egg', gif_add: 'https://i.pinimg.com/originals/fd/3c/cd/fd3ccd7b49e366b4206f5ac7f8fa8dac.gif')
+Gif.create(title: 'Homer Sip', gif_add: 'https://compote.slate.com/images/697b023b-64a5-49a0-8059-27b963453fb1.gif')
+Gif.create(title: 'Earth Moon Dawg', gif_add: 'https://blog.loomly.com/wp-content/uploads/2019/06/earth.gif')
+Gif.create(title: 'Hugz', gif_add: 'https://i.pinimg.com/originals/39/6d/8c/396d8cf19235b7b437d1067045a63b5a.gif')
+Gif.create(title: 'pik-achoo', gif_add: 'https://media0.giphy.com/media/3kzJvEciJa94SMW3hN/giphy.gif')
